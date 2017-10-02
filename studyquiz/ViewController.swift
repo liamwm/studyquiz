8b4f3f3ce01250ff3c1cb25063cdf9fa4fa11c61
Liam WM <liam.wm1@gmail.com>
2017-10-02 18:55:42 +1100

    Initial Commit

---
+++ studyquiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  studyquiz
+//
+//  Created by Liam Wilding-McBride on 2/10/17.
+//  Copyright © 2017 Liam Wilding-McBride. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
